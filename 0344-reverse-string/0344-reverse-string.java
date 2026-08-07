134d782df5bc3e422dffdb95f8af750479772ac8
Chitturi Naga Raja Teja Rohith <chitturirohith333@gmail.com>
2026-08-07 21:08:48 +0530

    Time: 1 ms (23.38%), Space: 48.1 MB (89.02%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left=0;
+        int n=s.length;
+        int right=n-1;
+        while(left<=right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
