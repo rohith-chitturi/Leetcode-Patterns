bdb160f4dcb42676bf6c7facc87cf905090c44ce
Chitturi Naga Raja Teja Rohith <chitturirohith333@gmail.com>
2026-08-08 13:02:09 +0530

    Time: 4 ms (99.05%), Space: 47.7 MB (48.31%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int n=s.length();
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            char ch1=s.charAt(left);
+            char ch2=s.charAt(right);
+            if(ch1==ch2){
+                left++;
+                right--;
+            }
+            else{
+                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+            }
+        }
+        return true;
+    }
+    private boolean isPalindrome(String s,int left,int right){
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
