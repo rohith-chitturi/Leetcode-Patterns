83c614f392d303ad3824f98ae4f12765f9c9124e
Chitturi Naga Raja Teja Rohith <chitturirohith333@gmail.com>
2026-08-16 08:52:24 +0530

    Time: 4 ms (75.68%), Space: 114.6 MB (82.43%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] counts=new int[3];
+        for(int stone:stones){
+            counts[stone%3]++;
+        }
+        if(counts[0]%2==0){
+            return counts[1]>0 && counts[2]>0;
+        }else{
+            return Math.abs(counts[1]-counts[2])>2;
+        }
+    }
+}
