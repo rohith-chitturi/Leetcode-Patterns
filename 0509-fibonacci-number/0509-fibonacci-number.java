42f60601eaffd5da2e4cd70cb1960f8fca98c30e
Chitturi Naga Raja Teja Rohith <chitturirohith333@gmail.com>
2026-08-23 12:06:15 +0530

    Time: 38 ms (9.64%), Space: 46.5 MB (16.15%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int[] arr=new int[n+1];
+        Arrays.fill(arr,-1);
+        if(n<=1){
+            return n;
+        }
+        if(arr[n]!=-1){
+            return arr[n];
+        }
+        return arr[n]=fib(n-1)+fib(n-2);
+    }
+}
