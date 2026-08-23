361af199340b3df92bdc1225df554b1000b95aa2
Chitturi Naga Raja Teja Rohith <chitturirohith333@gmail.com>
2026-08-23 16:24:26 +0530

    Time: 0 ms (100%), Space: 41.7 MB (97.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
        int[] dp=new int[n+1];
-       Arrays.fill(dp,-1);
-       return dynamicprogramming(n,dp); 
-    }
-    public int dynamicprogramming(int n,int[] dp){
-        if(n==1 || n==2 || n==3) return n;
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n]=dynamicprogramming(n-1,dp)+dynamicprogramming(n-2,dp);
+       if(n<=3) return n;
+       dp[0]=0;
+       dp[1]=1;
+       dp[2]=2;
+       dp[3]=3;
+       for(int i=4;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
     }
 }
