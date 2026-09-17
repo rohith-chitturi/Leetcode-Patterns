1ffdacb93a16f8bcea198c835d4c13af859e4df7
Chitturi Naga Raja Teja Rohith <chitturirohith333@gmail.com>
2026-09-17 15:07:40 +0530

    Time: 0 ms (100%), Space: 47.4 MB (48.68%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int xor=0;
+        for(int i=0;i<n;i++){
+            xor=xor^nums[i]^i;
+        }
+        return xor^n;
+    }
+}
